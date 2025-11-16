8c129ddbeea878341b74d46463bf859a6e160aab
Jerald Constantino <constantino.jeraldm@gmail.com>
2025-11-16 22:48:30 +0800

    fix tracking id counting

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ streamer = YoloByteTrackStreamer(
     device="cpu"
 )
 
-# SELECTED_CLASS_IDS = [0, 32]  # person, sports ball
-# streamer.run(source=0, target_classes=SELECTED_CLASS_IDS)
+SELECTED_CLASS_IDS = [0, 32]  # person, sports ball
+streamer.run(source=0, target_classes=SELECTED_CLASS_IDS)
 
-SELECTED_CLASS_IDS = [2, 3, 5, 7]  # car, motorcycle, bus, truck
-streamer.run(source="data/input/vehicles.mp4", 
-             target_classes=SELECTED_CLASS_IDS, 
-             output_path="data/output/vehicles_tracked.mp4")
+# SELECTED_CLASS_IDS = [2, 3, 5, 7]  # car, motorcycle, bus, truck
+# streamer.run(source="data/input/vehicles.mp4", 
+#              target_classes=SELECTED_CLASS_IDS, 
+#              output_path="data/output/vehicles_tracked.mp4")
